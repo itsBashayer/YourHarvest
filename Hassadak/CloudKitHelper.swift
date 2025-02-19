cb4413529dabc868d9fe89d4506f2a5a9d21860e
itsBashayer <alfuhaidbashayer@gmail.com>
2025-02-19 12:12:52 +0300

    Registration

---
+++ Hassadak/CloudKitHelper.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitHelper.swift
+//  Hassadak
+//
+//  Created by BASHAER AZIZ on 20/08/1446 AH.
+//
+
+import SwiftUI
+
+struct CloudKitHelper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitHelper()
+}
