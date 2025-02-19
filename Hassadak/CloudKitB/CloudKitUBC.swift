6edadae3f71b41b9f199896814fe5032402f7b1b
itsBashayer <alfuhaidbashayer@gmail.com>
2025-02-19 10:09:59 +0300

    CloudKit

---
+++ Hassadak/CloudKitB/CloudKitUBC.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitUBC.swift
+//  Hassadak
+//
+//  Created by BASHAER AZIZ on 20/08/1446 AH.
+//
+
+import SwiftUI
+
+struct CloudKitUBC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitUBC()
+}
