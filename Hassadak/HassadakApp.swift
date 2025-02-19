6443a3e972201d4f876b475801f2a16b7e9b497e
itsBashayer <alfuhaidbashayer@gmail.com>
2025-02-19 09:59:56 +0300

    Initial Commit

---
+++ Hassadak/HassadakApp.swift
@@ -0,0 +1,17 @@
+//
+//  HassadakApp.swift
+//  Hassadak
+//
+//  Created by BASHAER AZIZ on 20/08/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HassadakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
