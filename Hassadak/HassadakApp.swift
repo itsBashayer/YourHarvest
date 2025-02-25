8b3f7d87235f39228fdd0698bce9bda3bdf3757b
itsBashayer <alfuhaidbashayer@gmail.com>
2025-02-25 11:23:52 +0300

    History

--- Hassadak/HassadakApp.swift
+++ Hassadak/HassadakApp.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import Firebase
+
 @main
 struct HassadakApp: App {
     // Register app delegate for Firebase setup
@@ -22,7 +22,7 @@ struct HassadakApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+       
         return true
     }
 }
